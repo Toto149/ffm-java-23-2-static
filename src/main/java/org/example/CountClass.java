ca8dcc4759daff83865882e1e1d69a83de4ac2f4
Thorsten Thomann <thorsten.thomann@googlemail.com>
2023-11-08 15:26:01 +0100

    Initial Commit

--- src/main/java/org/example/CountClass.java
+++ src/main/java/org/example/CountClass.java
@@ -1,2 +1,25 @@
-package org.example;public class CountClass {
+package org.example;
+
+public class CountClass {
+
+    static int totalCount;
+    int instanceCount;
+
+    public CountClass(){
+
+    }
+    public CountClass(int instanceCount){
+        this.instanceCount = instanceCount;
+    }
+
+    public static void incrementTotalCount(){
+        totalCount++;
+    }
+    public void incrementInstanceCount(){
+        instanceCount++;
+    }
+
+    public String toString(){
+        return "{totalCount=%d, instanceCount=%d}".formatted(totalCount,instanceCount);
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,23 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        CountClass counter1 = new CountClass(2);
+        CountClass counter2 = new CountClass(3);
+        CountClass counter3 = new CountClass(-5);
+
+        System.out.println(counter1);
+        System.out.println(counter2);
+        System.out.println(counter3);
+        CountClass.incrementTotalCount();
+        CountClass.incrementTotalCount();
+        CountClass.incrementTotalCount();
+
+        counter1.incrementInstanceCount();
+        counter1.incrementInstanceCount();
+        counter1.incrementInstanceCount();
+        System.out.println(counter1);
+        System.out.println(counter2);
+        System.out.println(counter3);
+
     }
 }
